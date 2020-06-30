6d768f0c60548f14fbfeb6ff71f0d620418e0825
LAPTOP-HV0KE7LJ\Boris <figo858@hotmail.com>
2020-06-30 19:26:43 -0400

    ejemplo4

---
+++ src/ejercicio4.java
@@ -0,0 +1,10 @@
+public class ejercicio4 {
+    //Método que reciba dos enteros y retorne la multiplicación de la misma.
+    public static void main (String args []){
+        int multiplicador = multi(4,5);
+        System.out.println("el valor de la multiplicacion es  "+multiplicador);
+    }
+    public static int multi(int num1, int num2){
+        return (num1*num2);
+    }
+}
