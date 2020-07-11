b55563695be1597a2ea91f5ca8b35e09f47a70cf
LAPTOP-HV0KE7LJ\Boris <figo858@hotmail.com>
2020-07-11 11:03:21 -0400

    Practico 5, ejerccio 3

---
+++ src/Librería.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Librería {
+    public static void main(String args[]){
+        // Pedir los datos al usuario
+        Scanner input = new Scanner(System.in);
+        System.out.println("-----------------------Datos del libros----------------------------------");
+        System.out.println("Ingrese el isbn del libro 1  ");
+        int libro1ISBN = input.nextInt();
+
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el numero de paginas del libro 1  ");
+        int libro1Paginas = input.nextInt();
+
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el titulo del libro 1   ");
+        String  libro1Titulo= input.nextLine();
+
+         System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el autor del libro 1   ");
+        String  libro1Autor= input.nextLine();
+        System.out.println("------------------------------------------------------------------------------------");
+
+        //libro 2
+        System.out.println("Ingrese el isbn del libro 2  ");
+        int libro2ISBN = input.nextInt();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el titulo del libro 2   ");
+        String  libro2Titulo= input.nextLine();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el autor del libro 2   ");
+        String  libro2Autor= input.nextLine();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Ingrese el numero de paginas del libro 2  ");
+        int libro2Paginas = input.nextInt();
+        System.out.println("------------------------------------------------------------------------------------");
+
+        // mostrar libros en pantalla
+        Libro  libro1= new Libro(libro1ISBN,libro1Titulo,libro1Autor,libro1Paginas);
+        System.out.println("--------------------------Resumen del libro 1--------------------------------------");
+        System.out.println(libro1);
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("--------------------------Resumen del libro 2--------------------------------------");
+         Libro libro2= new Libro(libro2ISBN,libro2Titulo,libro2Autor,libro2Paginas);
+         System.out.println(libro2);
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("--------------------------Resumen del libro 3--------------------------------------");
+        Libro  libro3= new Libro();
+        System.out.println(libro3);
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+    //
+}
---
+++ src/Libro.java
@@ -0,0 +1,50 @@
+public class Libro {
+    //ISBN: int
+   // Titulo: String
+  //  Autor: String
+   // Número de páginas: int
+private int ISBN;
+private String Titulo;
+private String Autor;
+private int Numero_paginas;
+// constructor sin parametros
+public Libro(){
+    this.ISBN=0000001;
+    this.Titulo="Sin título";
+    this.Autor="Desconocido";
+    this.Numero_paginas=100;
+ }
+ // constructor con parametros
+  public Libro(int unIsbn, String unTitulo,String unAutor,int unNpagina){
+  this.ISBN=unIsbn;
+  this.Titulo=unTitulo;
+  this.Autor=unAutor;
+  this.Numero_paginas=unNpagina;
+  }
+  // metodo get
+   public String getTitulo(){ return this.Titulo;}
+   public  String getAutor (){return this.Autor;}
+   public int getNumero_paginas(){return this.Numero_paginas; }
+   public int getISBN(){return this.ISBN;}
+   // metodo set
+    public void setISBN(int unIsbn) { this.ISBN = unIsbn;}
+    public void setAutor(String unAutor) { this.Autor = unAutor;}
+    public void setTitulo(String unTitulo) { this.Titulo = unTitulo;}
+    public void setNumero_paginas(int unNpagina) { this.Numero_paginas =unNpagina ;}
+    //metodo de impresion
+    public String toString() {
+    return "El libro con  ISBN"+this.getISBN()+" creado por el autor" +this.getAutor() +" tiene "+this.getNumero_paginas()+" paginas";
+    }
+
+    //Si el autor está en null, se debe mostrar “desconocido”.
+    public String esDesconocido(String autores) {
+        if (autores == null) {
+            return "El autor es desconocido";
+        } else {
+            return "El autor es conocido";
+        }
+    }
+
+
+//
+}
